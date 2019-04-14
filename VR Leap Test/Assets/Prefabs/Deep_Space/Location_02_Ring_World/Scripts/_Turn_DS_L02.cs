08d8736cd760266dd28f9a87e4b97995b2fcfd22
Shan Memon <shankmemon@gmail.com>
2019-04-14 01:15:33 -0500

    Added space

---
+++ VR Leap Test/Assets/Prefabs/Deep_Space/Location_02_Ring_World/Scripts/_Turn_DS_L02.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _Turn_DS_L02 : MonoBehaviour {
+
+	public float SpeedY = 0.01f;
+	public float SpeedZ = 0.0f;
+
+	void Start () {
+		
+	}
+
+	void Update(){
+		transform.Rotate(0, SpeedY, SpeedZ);
+	}
+}
---
+++ VR Leap Test/Assets/Prefabs/Deep_Space/Location_02_Ring_World/Scripts/_UI_Flicker_L02.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class _UI_Flicker_L02 : MonoBehaviour {
+
+	public Image img;
+	public Image img2;
+
+	void Start () {
+
+		StartCoroutine(Disp());
+		img.enabled = false;
+		img2.enabled = false;
+	}
+
+	IEnumerator Disp()
+	{
+		while(true)
+		{
+			img.enabled = true;
+			img2.enabled = true;
+			yield return new WaitForSeconds(0.5f);
+			img.enabled = false;
+			img2.enabled = false;
+			yield return new WaitForSeconds(0.5f);
+			yield return null;
+	}
+}
+}
